a19f269d984dc028d72dd8ba5d94baa25ff0fb7a
mimihimesama <gwa446@gmail.com>
2024-05-31 19:46:26 +0900

    ✨ Update 축구게임 / 점수 조정 기능
    축구 게임 로직 설정, 결과에 따라 rank 테이블 생성
    
    resolves #7

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import accountRouter from "./routes/accounts.router.js";
+import playRouter from "./routes/play.router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", [accountRouter]);
+app.use("/api", [accountRouter, playRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸다.");
