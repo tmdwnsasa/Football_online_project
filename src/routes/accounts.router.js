621c6fdd17bd2029212925dba9b2ddae1d4b1119
PSY8937 <qkrtmdduq112@naver.com>
2024-05-31 23:05:01 +0900

    ✨ Update : accounts.router.js
    회원가입 API에 랭크 생성 추가
    
    score를 기준으로 내림차순 정렬하는 랭킹 시스팀 추가

--- src/routes/accounts.router.js
+++ src/routes/accounts.router.js
@@ -1,14 +1,13 @@
 import express from "express";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { accountPrisma } from "../utils/prisma/index.js";
-import authMiddleware from '../middlewares/auth.middleware.js';
-
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 // 회원가입 API
-router.post('/accounts/sign-up', async (req, res, next) => {
+router.post("/accounts/sign-up", async (req, res, next) => {
   try {
     const { id, password, passwordConfirm, nickname } = req.body;
 
@@ -17,48 +16,56 @@ router.post('/accounts/sign-up', async (req, res, next) => {
         id,
       },
     });
-    
+
     if (isExistAccount) {
-      return res.status(400).json({ message: '이미 존재하는 아이디입니다.' });
+      return res.status(400).json({ message: "이미 존재하는 아이디입니다." });
     }
 
     const vaildIdname = /^[a-z0-9]+$/;
     if (!vaildIdname.test(id)) {
-      return res
-        .status(400)
-        .json({ message: '아이디는 영어와 숫자만 사용할 수 있습니다.' });
+      return res.status(400).json({ message: "아이디는 영어와 숫자만 사용할 수 있습니다." });
     }
 
     if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: '비밀번호는 최소 6자 이상이어야 합니다.' });
+      return res.status(400).json({ message: "비밀번호는 최소 6자 이상이어야 합니다." });
     }
 
     if (password !== passwordConfirm) {
-      return res
-        .status(400)
-        .json({ message: '비밀번호와 비밀번호 확인이 일치하지 않습니다.' });
+      return res.status(400).json({ message: "비밀번호와 비밀번호 확인이 일치하지 않습니다." });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const account = await accountPrisma.account.create({
-      data: {
-        id,
-        password: hashedPassword,
-        nickname,
-      },
+    await accountPrisma.$transaction(async (tx) => {
+      await tx.account.create({
+        data: {
+          id,
+          password: hashedPassword,
+          nickname,
+        },
+      });
+
+      const createdNow = await tx.account.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      await tx.rank.create({
+        data: {
+          account_id: createdNow.account_id,
+        },
+      });
     });
 
-    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+    return res.status(201).json({ message: "회원가입이 완료되었습니다." });
   } catch (err) {
     next(err);
   }
 });
 
 // 로그인 API
-router.post('/accounts/sign-in', async (req, res, next) => {
+router.post("/accounts/sign-in", async (req, res, next) => {
   try {
     const { id, password } = req.body;
 
@@ -68,19 +75,18 @@ router.post('/accounts/sign-in', async (req, res, next) => {
       },
     });
 
-    if (!isExistAccount)
-      return res.status(401).json({ message: '존재하지 않는 아이디입니다.' });
+    if (!isExistAccount) return res.status(401).json({ message: "존재하지 않는 아이디입니다." });
     else if (!(await bcrypt.compare(password, isExistAccount.password)))
-      return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
 
     const accessToken = jwt.sign(
       { account_id: isExistAccount.account_id },
       process.env.ACCESS_TOKEN_SECRET_KEY,
-      { expiresIn: '1h' }
+      { expiresIn: "1h" },
     );
 
-    res.cookie('authorization', `Bearer ${accessToken}`);
-    return res.status(200).json({ message: '로그인 성공' });
+    res.cookie("authorization", `Bearer ${accessToken}`);
+    return res.status(200).json({ message: "로그인 성공" });
   } catch (error) {
     next(error);
   }
@@ -123,11 +129,63 @@ router.patch("/accounts/:id/buy-cash", authMiddleware, async (req, res, next) =>
       },
     });
 
-    res.status(200).json( responseCash );
+    res.status(200).json(responseCash);
   } catch (error) {
     next(error);
   }
 });
 
+// 승률 계산 함수
+const getOdds = (win, draw, lose) => {
+  let totalGame = win + draw + lose;
+  let winrates = Math.floor(((win + draw * 0.5) / totalGame) * 100);
+  if (totalGame === 0) winrates = 0;
+  return winrates;
+};
+
+// 랭킹 조회 API
+router.get("/accounts/rank", async (req, res, next) => {
+  try {
+    const accountRank = await accountPrisma.rank.findMany({
+      select: {
+        account_id: true,
+        score: true,
+        win: true,
+        draw: true,
+        lose: true,
+      },
+      orderBy: {
+        score: "desc",
+      },
+    });
+
+    const response = [];
+
+    for (const account of accountRank) {
+      const accountId = await accountPrisma.account.findFirst({
+        where: {
+          account_id: account.account_id,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      const odds = getOdds(account.win, account.draw, account.lose);
+      response.push({
+        id: accountId.id,
+        score: account.score,
+        odds: odds,
+        win: account.win,
+        draw: account.draw,
+        lose: account.lose,
+      });
+    }
+
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
