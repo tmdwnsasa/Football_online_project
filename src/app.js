222f6c0b87ba4464aefbf0dfd18e423d21148823
TalpangEE <https://github.com/TalpangEE>
2024-05-31 18:49:32 +0900

    🩹 Fix  character -> player 및 prisma model 변경
    전체 코드에서 character -> player
    prisma model 수정 (rank 테이블, win-draw-lose 위취변경, level 추가, 복합키 설정)
    
    resolves #5

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import accountRouter from "./routes/accounts.router.js";
-import characterData from "./routes/character.data.router.js";
-
+import playerData from "./routes/player.data.router.js";
+import makedata from "./srcipt/makedata.js";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", [accountRouter,characterData]);
+app.use("/api", [accountRouter,playerData]);
+
+makedata();
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸다.");
