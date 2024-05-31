75cf0d6427da2d7764f3f0d23c3d4932775c79ba
unknown <https://github.com/TalpangEE>
2024-05-31 16:32:11 +0900

    � Add character.data.json, character.data.router.js
    캐릭터 데이터 넣어보고
    랜덤 뽑기 구현중
    app.js에 import caharacter.data.rotuer.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import accountRouter from "./routes/accounts.router.js";
+import characterData from "./routes/character.data.router.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use("/api", [accountRouter]);
+app.use("/api", [accountRouter,characterData]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트로 서버가 열렸다.");
